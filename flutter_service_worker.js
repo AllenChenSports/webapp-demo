24cc7eb88afa85f34ac9ff215449fdd72d97c07e
AllenChenSports <haoting.chen.biz@gmail.com>
2023-01-21 11:03:37 +0900

    new commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -19,9 +19,9 @@ const RESOURCES = {
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
-"index.html": "0c43c353b17a378d888bd815d3058405",
-"/": "0c43c353b17a378d888bd815d3058405",
-"main.dart.js": "2ba4b843342d8e595ddf3c5da13d5819",
+"index.html": "f5ee9fa74146fdbb4d83c1d3e5f90c56",
+"/": "f5ee9fa74146fdbb4d83c1d3e5f90c56",
+"main.dart.js": "2b16472313233b25651f9cc58a47f565",
 "manifest.json": "bf24c84c3bf99672a631c4f84464e793",
 "version.json": "15235b5108d6a877ef74fe3317a96bf7"
 };
